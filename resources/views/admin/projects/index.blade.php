1d5a91e227e9802cf67108780d2046dca65a6161
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-06 10:54:17 +0200

    fix path + add destroy modal

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -40,22 +40,58 @@
 
                             <div class="d-flex gap-2">
                                 <button class="btn btn-outline-success">
-                                    <a class="text-white" href="{{ route('admin.projects.show', $proj->id) }}"
+                                    <a class="text-white" href="{{ route('admin.projects.show', $proj->slug) }}"
                                         title="Show">
                                         <i class="fa fa-eye" aria-hidden="true"></i>
                                     </a>
                                 </button>
 
                                 <button class="btn text-white btn-outline-warning">
-                                    <a class="text-white" href="{{ route('admin.projects.edit', $proj->id) }}"
+                                    <a class="text-white" href="{{ route('admin.projects.edit', $proj->slug) }}"
                                         title="Edit">
                                         <i class="fa fa-pencil" aria-hidden="true"></i>
                                     </a>
                                 </button>
 
-                                <button class="btn text-white btn-outline-danger">
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn text-white btn-outline-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $proj->id }}">
                                     <i class="fa fa-trash" aria-hidden="true"></i>
                                 </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade text-black" id="modalId-{{ $proj->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId-{{ $proj->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitle-{{ $proj->id }}">
+                                                    Delete
+                                                    {{ $proj->name }}
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Remove {{ $proj->name }}?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary rounded-pill"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <form action="{{ route('admin.projects.destroy', $proj->slug) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit"
+                                                        class="btn btn-danger rounded-pill">Confirm</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                         </td>
