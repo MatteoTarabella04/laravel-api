ae2b455663a06e20c20c12b1fe93c8939778df8e
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-07 17:21:53 +0200

    add route for types

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TypeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,10 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters([
         'projects' => 'project:slug'
+    ]);
+
+    Route::resource('types', TypeController::class)->parameters([
+        'types' => 'type:slug'
     ]);
 });
 
