3569daa13d2359b29381bab974c4489e81f7d3a4
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-08 15:56:31 +0200

    fix + authorize

---
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Technology;
+use App\Http\Requests\StoreTechnologyRequest;
+use App\Http\Requests\UpdateTechnologyRequest;
+use App\Http\Controllers\Controller;
+
+class TechnologyController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreTechnologyRequest $request)
+    {
+        //
+    }
+
+    public function show(Technology $technology)
+    {
+        //
+    }
+
+    public function edit(Technology $technology)
+    {
+        //
+    }
+
+    public function update(UpdateTechnologyRequest $request, Technology $technology)
+    {
+        //
+    }
+
+    public function destroy(Technology $technology)
+    {
+        //
+    }
+}
