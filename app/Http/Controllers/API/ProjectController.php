a03733143b09054036f7a0435799075f62bb90d0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-13 12:56:21 +0200

    add  API

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::orderByDesc('id')->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects,
+        ]);
+    }
+}
