77cc60e3a0e481d7ea36c6a28d59c9b3630fcd3b
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-16 10:21:03 +0200

    add show

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -10,11 +10,28 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with(['types', 'technologies', 'user'])->orderByDesc('id')->paginate(5);
+        $projects = Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(5);
 
         return response()->json([
             'success' => true,
             'projects' => $projects,
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with(['type', 'technologies'])->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'result' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => 'Page not found | 404',
+            ]);
+        }
     }
 }
